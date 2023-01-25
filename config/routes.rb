711cd3d37fe2d61d076cc1cbf6c93769faf072d2
EC2 Default User <120617980+zvyujg@users.noreply.github.com>
2023-01-25 13:30:28 +0000

    Second Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get'/top' =>'homes#top'
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about',as:'about'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books, only: [:new, :index, :show]
+  resources :books, only: [:new, :index, :show,:edit,:destroy,:create]
   resources :users, only: [:show, :edit]
 end
