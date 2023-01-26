417d74e556bc7d5fcc0d4d28ed4af48995c214b6
EC2 Default User <120617980+zvyujg@users.noreply.github.com>
2023-01-26 12:34:40 +0000

    Three Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,35 @@
 class UsersController < ApplicationController
+  def new 
+   @book = Book.new
+  end
+  
+  def index
+    @books =Book.all
+    @user = current_user
+  end
+
   def show
+    @user =User.find(params[:id])
   end
 
   def edit
+    @user =User.find(params[:id])
   end
+  
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+    flash[:notice] ="Book was successfully updated."
+    redirect_to user_path(@book)
+    else
+    render :edit
+    end
+  end
+  
+  private
+   def user_params
+     params.require(:user).permit(:name, :introduction,:profile_image)
+   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :books, only: [:new, :index, :show,:edit,:destroy,:create]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :index]
 end
