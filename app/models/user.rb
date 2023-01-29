e32c867335b199673946f1e93bb45ac5c7f934b4
EC2 Default User <120617980+zvyujg@users.noreply.github.com>
2023-01-29 10:57:29 +0000

    Ereven Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 has_many :books, dependent: :destroy
-validates :name,presence:true,length: {minimum: 2, maximum: 20}
+validates :name, presence:true, length: {minimum: 2, maximum: 20}
 validates :name, uniqueness: true
 validates :introduction, length: {maximum: 50}
 
